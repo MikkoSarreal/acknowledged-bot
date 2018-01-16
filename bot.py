f5d7eb42837881393abbfabe6ad800901f6b2208
Wenzel Lowe <lenzelwowe@gmail.com>
2018-01-15 18:19:59 -0800

    test

--- bot.py
+++ bot.py
@@ -18,3 +18,62 @@ r = redis.from_url(os.environ.get("REDIS_URL"))
 # bot = telebot.TeleBot(token)
 # some_api = some_api_lib.connect(some_api_token)
 #              ...
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
+
+
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+
+def new(bot, update):
+
+    
+    keyboard = [[InlineKeyboardButton("Acknowledge", callback_data="1")]]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    update.message.reply_text(update.message.text[4:] + '\n\nAcknowledged:\n', reply_markup=reply_markup)
+    
+
+def button(bot, update):
+    query = update.callback_query
+
+    
+    new_name = '-'+query.from_user['first_name']+' '+query.from_user['last_name']
+
+    if new_name in query.message.text:
+        new_text = query.message.text + ' '
+    else:
+        new_text =  query.message.text + '\n' + new_name
+
+    ackd_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Acknowledge", callback_data="2")]])
+    
+    bot.edit_message_text(reply_markup= ackd_markup , chat_id=query.message.chat_id, message_id=query.message.message_id, text = new_text)
+
+    
+    
+def error(bot, update, error):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+
+
+def main():
+    updater = Updater(token=token)
+
+    updater.dispatcher.add_handler(CommandHandler('new', new))
+    updater.dispatcher.add_handler(CallbackQueryHandler(button))
+    updater.dispatcher.add_error_handler(error)
+
+
+
+    updater.start_polling()
+
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
